dcf7c7e224671c86035f1a6ff75ee1b5e17df591
Alex Shevlyakov <sanchosrancho@gmail.com>
2018-08-10 19:30:52 +0300

    Add nyan cat tracking

--- ARPhotoFrame/ViewController.swift
+++ ARPhotoFrame/ViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 import SceneKit
 import ARKit
+import Foundation
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     @IBOutlet var sceneView: ARSCNView!
+    
+    // Create video player
+    let videoPlayer: AVPlayer = {
+        // Load cat video from bundle
+        guard let url = Bundle.main.url(forResource: "nyan-cat", withExtension: "mp4", subdirectory: "art.scnassets") else {
+            print("Could not find video file")
+            return AVPlayer()
+        }
+        
+        return AVPlayer(url: url)
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,22 +32,26 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
         // Set the scene to the view
-        sceneView.scene = scene
+        sceneView.scene = SCNScene()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
+        let configuration = ARImageTrackingConfiguration()
+        
+        // Load images from asset catalog
+        guard let trackingImages = ARReferenceImage.referenceImages(inGroupNamed: "Photos", bundle: Bundle.main) else {
+            print("Could not load images")
+            return
+        }
+        
+        // Setup configuration
+        configuration.trackingImages = trackingImages
+        configuration.maximumNumberOfTrackedImages = 1
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -46,17 +62,36 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-
+    
     // MARK: - ARSCNViewDelegate
     
-/*
+    
     // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         let node = SCNNode()
-     
+        
+        // Show video overlayed to image
+        if let imageAnchor = anchor as? ARImageAnchor {
+            
+            // Create a plane
+            let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width,
+                                 height: imageAnchor.referenceImage.physicalSize.height)
+            
+            // Set AVPlayer as the plane's texture and play
+            plane.firstMaterial?.diffuse.contents = self.videoPlayer
+            self.videoPlayer.play()
+            
+            let playNode = SCNNode(geometry: plane)
+            
+            // Rotate the plane to match the anchor
+            playNode.eulerAngles.x = -.pi / 2
+            
+            // Add plane node to parent
+            node.addChildNode(playNode)
+        }
+        
         return node
     }
-*/
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
